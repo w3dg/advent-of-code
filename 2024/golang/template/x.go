228bc6bc845e902cba06061e52af25a0da8fe765
w3dg <51162936+w3dg@users.noreply.github.com>
2025-07-14 13:42:52 +0530

    refactor(template): update aoc template to use libs

--- 2024/golang/template/x.go
+++ 2024/golang/template/x.go
@@ -1,38 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
+
+	aoc "github.com/w3dg/aocutils"
+	_ "github.com/zyedidia/generic"
 )
-
-func ReadInputLines(name string) ([]string, error) {
-	file, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-
-	lines := []string{}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return lines, nil
-}
-
-func num(s string) int {
-    n, err := strconv.Atoi(s)
-    if err != nil {
-        panic("cannot convert to int")
-    }
-
-    return n
-}
 
 func p1(lines []string) int {
 	return 0
@@ -48,7 +23,7 @@ func main() {
 	if len(arg) != 0 {
 		infile = arg[0]
 	}
-	lines, err := ReadInputLines(infile)
+	lines, err := aoc.ReadFileLines(infile)
 	if err != nil {
 		log.Fatal("cannot read")
 	}
